9a45dc0f098e9f4c68154c8d085ef8be1bfd57b3
Kartones <diego@kartones.net>
2016-12-26 17:45:33 +0100

    no module-level vars/instances

--- twitter-purge/twitter-purge.py
+++ twitter-purge/twitter-purge.py
@@ -3,23 +3,22 @@ import tweepy
 import config
 
 
-# http://docs.tweepy.org/en/v3.5.0/api.html
-auth = tweepy.OAuthHandler(config.CONSUMER_KEY, config.CONSUMER_SECRET)
-auth.set_access_token(config.TOKEN, config.TOKEN_SECRET)
-api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-
-
 def process():
+    auth = tweepy.OAuthHandler(config.CONSUMER_KEY, config.CONSUMER_SECRET)
+    auth.set_access_token(config.TOKEN, config.TOKEN_SECRET)
+    # http://docs.tweepy.org/en/v3.5.0/api.html
+    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
     user = api.me()
-    tweets = fetch_tweets(user.screen_name)
-    purge_old_tweets(tweets)
+    tweets = fetch_tweets(api, user.screen_name)
+    purge_old_tweets(api, tweets)
 
 
-def fetch_tweets(user_screen_name):
+def fetch_tweets(api, user_screen_name):
     return api.user_timeline(screen_name=user_screen_name, count=100, include_rts=True)
 
 
-def purge_old_tweets(tweets):
+def purge_old_tweets(api, tweets):
     max_keep_date = datetime.today() + timedelta(-config.DAYS_TO_KEEP)
     for tweet in tweets:
         if tweet.created_at < max_keep_date:
