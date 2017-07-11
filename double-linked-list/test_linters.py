475a89de6186f79200697997f965ec9a8a15175d
Kartones <diego@kartones.net>
2017-07-11 18:41:38 +0200

    Improve subprocess call binary location

--- double-linked-list/test_linters.py
+++ double-linked-list/test_linters.py
@@ -2,8 +2,15 @@ import subprocess
 import sys
 
 
+SOURCE_FOLDER = "."
+
+
 def test_flake8_compliance() -> None:
-    result = subprocess.call("/usr/local/bin/flake8 .",
+    flake8_binary = subprocess.check_output("which flake8",
+                                            shell=True,
+                                            stderr=sys.stderr).decode("ascii").replace("\n", "")
+
+    result = subprocess.call("{} {}".format(flake8_binary, SOURCE_FOLDER),
                              shell=True,
                              stdout=sys.stdout,
                              stderr=sys.stderr)
@@ -11,9 +18,13 @@ def test_flake8_compliance() -> None:
 
 
 def test_mypy_compliance() -> None:
-    result = subprocess.call("/usr/local/bin/mypy --ignore-missing-imports --disallow-untyped-defs --check-untyped-defs"
-                             " --warn-redundant-casts --warn-return-any"
-                             " --warn-unused-ignores --strict-optional .",
+    mypy_binary = subprocess.check_output("which mypy",
+                                          shell=True,
+                                          stderr=sys.stderr).decode("ascii").replace("\n", "")
+
+    result = subprocess.call("{} --ignore-missing-imports --disallow-untyped-defs --check-untyped-defs"
+                             " --warn-redundant-casts --warn-return-any --warn-unused-ignores --strict-optional"
+                             " {}".format(mypy_binary, SOURCE_FOLDER),
                              shell=True,
                              stdout=sys.stdout,
                              stderr=sys.stderr)
