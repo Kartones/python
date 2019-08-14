3fa9dc822f0222b55d17460f5d1e72ca49c2ad3c
Kartones <diego@kartones.net>
2019-08-14 12:43:11 +0200

    ImageCache object for Pelican deferred image loading plugin

--- pelican/plugins/defer_loading_images/defer_loading_images.py
+++ pelican/plugins/defer_loading_images/defer_loading_images.py
@@ -16,63 +16,64 @@ import requests
 from pelican import signals
 
 
-CACHE_FOLDER_NAME = "img_cache"
-CACHE_FILE_NAME = "cache.json"
+class ImageCache():
 
+    CACHE_FOLDER_NAME = "img_cache"
+    CACHE_FILE_NAME = "cache.json"
+    # TODO: Move to settings, but what default value to provide if not present?
+    MAX_WIDTH = 748
 
-# TODO: Move to settings, but what default value to provide if not present?
-MAX_WIDTH = 748
+    def __init__(self):
+        self.cache = {}
+        self.dirty = False
 
+    def load(self, instance):
+        cache_file = self._get_cache_file_path(instance)
 
-def _get_cache_file_path(instance):
-    base_path = os.path.dirname(instance.settings["PATH"])
-    cache_path = os.path.join(base_path, CACHE_FOLDER_NAME)
+        if os.path.exists(cache_file):
+            with open(cache_file, "r") as file_handle:
+                self.cache = json.load(file_handle)
 
-    if not os.path.exists(cache_path):
-        os.mkdir(cache_path)
-        print("Created image cache folder '{}'".format(cache_path))
+    def save(self, instance):
+        if not self.dirty:
+            return
 
-    return os.path.join(cache_path, CACHE_FILE_NAME)
+        cache_file = self._get_cache_file_path(instance)
 
+        with open(cache_file, "w") as file_handle:
+            json.dump(self.cache, file_handle)
 
-def _load_image_cache(instance):
-    cache_file = _get_cache_file_path(instance)
+    def get_image_width_and_height(self, image_path):
+        if image_path in self.cache.keys():
+            width, height = self.cache[image_path]
+        else:
+            print("Caching '{}'".format(image_path))
+            request = requests.get(image_path)
+            image = Image.open(BytesIO(request.content))
+            width, height = image.size
+            image.close()
 
-    if os.path.exists(cache_file):
-        with open(cache_file, "r") as file_handle:
-            cache = json.load(file_handle)
-    else:
-        cache = {}
+            self.cache[image_path] = (width, height)
+            self.dirty = True
 
-    return cache
+        if width > self.MAX_WIDTH:
+            # recalculate proportions and restrain image size
+            original_proportion = width / height
+            width = self.MAX_WIDTH
+            height = int(width / original_proportion)
 
+        return (width, height)
 
-def _save_image_cache(instance, cache):
-    cache_file = _get_cache_file_path(instance)
+    @classmethod
+    def _get_cache_file_path(cls, instance):
+        base_path = os.path.dirname(instance.settings["PATH"])
+        cache_path = os.path.join(base_path, cls.CACHE_FOLDER_NAME)
 
-    with open(cache_file, "w") as file_handle:
-        cache = json.dump(cache, file_handle)
+        if not os.path.exists(cache_path):
+            os.mkdir(cache_path)
+            print("Created image cache folder '{}'".format(cache_path))
 
-
-def _get_image_width_and_height(image_path, cache):
-    if image_path in cache.keys():
-        width, height = cache[image_path]
-    else:
-        print("Caching '{}'".format(image_path))
-        request = requests.get(image_path)
-        image = Image.open(BytesIO(request.content))
-        width, height = image.size
-        image.close()
-
-        cache[image_path] = (width, height)
-
-    if width > MAX_WIDTH:
-        # recalculate proportions and restrain image size
-        original_proportion = width / height
-        width = MAX_WIDTH
-        height = int(width / original_proportion)
-
-    return (width, height)
+        return os.path.join(cache_path, cls.CACHE_FILE_NAME)
 
 
 def content_object_init(instance):
@@ -80,7 +81,8 @@ def content_object_init(instance):
     if instance._content is None:
         return
 
-    image_cache = _load_image_cache(instance)
+    image_cache = ImageCache()
+    image_cache.load(instance)
 
     content = instance._content
     soup = BeautifulSoup(content, "html.parser")
@@ -96,7 +98,7 @@ def content_object_init(instance):
             continue
 
         # Always define width & height
-        width, height = _get_image_width_and_height(img["src"], image_cache)
+        width, height = image_cache.get_image_width_and_height(img["src"])
         img["width"] = img.get("width", width)
         img["height"] = img.get("height", height)
 
@@ -111,7 +113,7 @@ def content_object_init(instance):
         if img.get("mce_src"):
             del img["mce_src"]
 
-    _save_image_cache(instance, image_cache)
+    image_cache.save(instance)
 
     instance._content = soup.decode()
 
