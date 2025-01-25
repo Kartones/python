835c95cf51dfc46d11f01fc9ae5a00ecbbfea939
Kartones <diego@kartones.net>
2025-01-25 21:30:44 +0100

    fix: make markov model use word frequencies

--- markov-model/markov_model.py
+++ markov-model/markov_model.py
@@ -1,6 +1,9 @@
 """
 Given a list of sentences, the markov model takes note of how often it witnesses a transition from one word to another.
 
+Frequency is represented by the number of times that a word is stored in the list of possible next words
+for a given word. This wastes space but it is a very simple implementation.
+
 SOLUTION NOTE:
 While using sets for the values removes some repetition, in order to really save memory the way to go would be to build
 a word dictionary and make the model store dictionary indexes, so each word really appears just once and the sets
@@ -9,44 +12,46 @@ contain only integers (instead of strings as now)
 """
 from collections import defaultdict
 from random import choice
-from typing import (Dict, List, Optional, Set)
+from typing import (Dict, List, Optional)
 
-
+# The model stores which words are used as sentence start and end delimiters.
+# It will only begin a sentence with a start word, and will end if picking an end word even if not yet reached the
+# desired maximum sentence length.
 SENTENCE_START_DELIMITER = "@@"
 SENTENCE_END_DELIMITER = "||"
 
 
 def build_model(sentences: List[str]) -> Dict:
-    model = defaultdict(set)  # type: Dict[str, Set]
-
-    # NOTE: explicitly removing repetitions, as `love -> love` in `Do what you love, love what you do`
-    # but if input sentences have them, an alternative is to leave them as valid
+    model = defaultdict(list)  # type: Dict[str, list]
 
     for sentence in sentences:
         words = sentence.split(" ")
         if len(words) < 2:
             raise ValueError("Sentences must be at least two words!")
         for index, word in enumerate(words):
-            if index == 0:
-                model[word].add(SENTENCE_START_DELIMITER)
-                if words[index+1] not in model[word]:
-                    model[word].add(words[index+1])
-            elif index == len(words) - 1:
-                model[word].add(SENTENCE_END_DELIMITER)
-            else:
-                if words[index+1] not in model[word]:
-                    model[word].add(words[index+1])
+            # TODO: handle case of 1-word sentences
+            if index == len(words) - 1:
+                if SENTENCE_END_DELIMITER not in model[word]:
+                    model[word].append(SENTENCE_END_DELIMITER)
+                continue
+
+            if index == 0 and SENTENCE_START_DELIMITER not in model[word]:
+                model[word].append(SENTENCE_START_DELIMITER)
+
+            # explicitly removing direct repetitions, as `love -> love` in `Do what you love, love what you do`
+            if words[index+1] != word:
+                model[word].append(words[index+1])
 
     return model
 
 
-def build_markov_sentence(model: Dict[str, Set], max_words: int) -> str:
+def build_markov_sentence(model: Dict[str, List], max_words: int) -> str:
     words = do_build_markov_sentence(current_word=None, model=model, word_count=0, max_words=max_words)
     return " ".join(words)
 
 
 def do_build_markov_sentence(
-    current_word: Optional[str], model: Dict[str, Set], word_count: int, max_words: int
+    current_word: Optional[str], model: Dict[str, List], word_count: int, max_words: int
 ) -> List[str]:
     # 1st word scenario: find a valid sentence start
     if current_word is None:
@@ -95,6 +100,11 @@ def _load_sentences(filename: str) -> List[str]:
 
 
 if __name__ == '__main__':
+
+    # trivial example (model very simple to understand if printed)
+    # filename = "simple_test.txt"
+    # max_words = 5
+
     # simple example using some quotes
     filename = "markov_quotes.txt"
     max_words = 20
